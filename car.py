7be6d16fe4cb217e3abfad62de22b9ed13477b47
owaisv <owaisvalli@gmail.com>
2023-07-11 20:47:07 +0100

    Changes to be committed: 	modified:   car.py

--- car.py
+++ car.py
@@ -1,13 +1,16 @@
+from engine import Engine
+from battery import Battery
+
 class Car:
     def __init__(self, engine, battery):
         self.engine = engine
         self.battery = battery
     
     def engineShouldBeServiced(self):
-        pass
+        return self.engine.engineShouldBeServiced()
 
     def batteryShouldBeServiced(self):
-        pass
+        return self.battery.batteryShouldBeServiced()
 
     def needsService(self):
         pass
